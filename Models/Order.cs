f75edec8e3ec561182f275ddaa1f0016e347de03
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-02-25 20:00:25 -0500

    feat: Created orders/order item table

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace nooblion_commerce_api.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderCompleted { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+
+        //Eventually these will be more standard like ISO codes for the country that will be stored in another table
+        public required string Country { get; set; }
+        public required string Street { get; set; }
+        public required string City { get; set; }
+    }
+}
