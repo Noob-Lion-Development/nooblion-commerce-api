e9c9177533bcba7cfbaeb7e09dcf15ebe3633e3b
Trevor Hedgpeth <trevorthebeast1217@gmail.com>
2023-03-05 13:16:08 -0500

    fix: orderItem model fixed

---
+++ DataObjects/OrderItemObj.cs
@@ -0,0 +1,9 @@
+﻿namespace nooblion_commerce_api.DataObjects
+{
+    public class OrderItemObj
+    {
+        public int productId { get; set; }
+        public int orderId { get; set; }
+        public int quantity { get; set; }
+    }
+}
